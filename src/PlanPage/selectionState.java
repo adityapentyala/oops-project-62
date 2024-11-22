582ba0e7e69d73b8aca2df08c8810f1171587317
Aditya Pentyala <adityapentyala02@gmail.com>
2024-11-22 09:42:48 +0530

    feat: add filename field to selectionState

--- src/PlanPage/selectionState.java
+++ src/PlanPage/selectionState.java
@@ -7,4 +7,5 @@ public class selectionState {
     public static Map<String, Integer> selection = new HashMap();
     public static Map<Integer, Color> colorMap = new HashMap();
     public static Room selectedRoom = null;
+    public static String filename = null; 
 }
